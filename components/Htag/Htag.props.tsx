019c440965c3b21ba874799de402c67d08ffe4ee
dsgnfox <dsgnfox@gmail.com>
2022-07-21 15:55:51 +0300

    add components H & P & Tag

--- components/Htag/Htag.props.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from 'react';
-
-export interface IHtagProps {
-  tag: 'h1' | 'h2' | 'h3';
-  children: ReactNode
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
-import { Htag } from '../components';
-import { Button } from '../components';
+import { H, Button, P, Tag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
     <div>
-      <Htag tag='h1'>Hello!</Htag>
+      <H tag='h1' title='123123'>Hello!</H>
       <Button apperance='primary'>button</Button>
       <Button apperance='ghost' arrow='down'>button with arrow</Button>
+      <P size='s'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora perspiciatis neque velit earum qui iure voluptates molestias quam omnis aliquam, et eaque. Totam consequatur, quisquam at non possimus eius velit!</P>
+      <P size='l'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora perspiciatis neque velit earum qui iure voluptates molestias quam omnis aliquam, et eaque. Totam consequatur, quisquam at non possimus eius velit!</P>
+      <Tag size='m' color='green' href='http://localhost:3000/' target='_blank'>Tag</Tag>
     </div>
   );
 }
