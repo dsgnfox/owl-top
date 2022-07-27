205e2a01c842f261192e493b7d17842bf4ca975b
dsgnfox <dsgnfox@gmail.com>
2022-07-27 17:29:27 +0300

    create static pages

---
+++ interfaces/menu.interface.ts
@@ -0,0 +1,18 @@
+export interface Id {
+  secondCategory: string;
+}
+
+export interface PageItem {
+  alias: string;
+  title: string;
+  _id: string;
+  category: string;
+}
+
+export interface MenuItem {
+  _id: {
+    secondCategory: string;
+  };
+  pages: PageItem[];
+}
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import { GetStaticProps } from 'next';
 import { useState } from 'react';
 import { H, Button, P, Tag, Rating } from '../components';
 import { withLayout } from '../layout/Layout';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
 
   const [rating, setRating] = useState<number>(4);
 
@@ -15,8 +18,31 @@ function Home(): JSX.Element {
       <P size='l'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora perspiciatis neque velit earum qui iure voluptates molestias quam omnis aliquam, et eaque. Totam consequatur, quisquam at non possimus eius velit!</P>
       <Tag size='m' color='green' href='http://localhost:3000/' target='_blank'>Tag</Tag>
       <Rating rating={rating} isEditable={true} setRating={setRating} />
+      <ul>
+        {menu.map((m) => <li key={m._id.secondCategory}>{m._id.secondCategory}</li>)}
+      </ul>
     </div>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory
+  });
+
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[],
+  firstCategory: number
+}
