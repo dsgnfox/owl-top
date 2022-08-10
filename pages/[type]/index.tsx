02e295dce597ed960a22061e4f8521b59bf9b1b5
dsgnfox <dsgnfox@gmail.com>
2022-08-10 12:31:03 +0300

    some fixes

--- pages/[type]/index.tsx
+++ pages/[type]/index.tsx
@@ -3,7 +3,7 @@ import { withLayout } from '../../layout/Layout';
 import axios from 'axios';
 import { MenuItem } from '../../interfaces/menu.interface';
 import { firstLevelMenu } from '../../helpers/helpers';
-import { ParsedUrlQuery } from 'node:querystring';
+import { ParsedUrlQuery } from 'querystring';
 import {API} from "../../helpers/api";
 
 function Type({ firstCategory }: TypeProps): JSX.Element {
